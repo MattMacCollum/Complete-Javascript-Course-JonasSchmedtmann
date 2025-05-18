ae68dd7b98d4c518201d0c50b9e58f2a778d0ab3
Matt Mc Collum <mmccollum08@qub.ac.uk>
2025-05-18 20:20:19 +0100

    Updated script.js with score conditions

--- Pig Dice Game/script.js
+++ Pig Dice Game/script.js
@@ -3,9 +3,44 @@
 // Getting the elements of the scores
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
-const diceEl = document.querySelector('.dice')
+const curren0El = document.getElementById('current--0');
+const curren1El = document.getElementById('current--1');
 
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+
+
+//Initial conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
+btnRoll.addEventListener('click', function(){
+  //Generate random dice roll
+  const dice = Math.trunc(Math.random() * 6)
+
+  //Display the corresponding dice face
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //Check if rolled a 1: if true, switch player
+  
+  
+  if(dice !== 1){
+    //Case if dice isnt 1: Add to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+
+  }else{
+
+  }
+
+
+
+})
