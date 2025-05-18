dc822dd67afb4327f889613cf069bbdf2c2c0550
Matt Mc Collum <mmccollum08@qub.ac.uk>
2025-05-18 20:08:19 +0100

    Started working on dice elements
    Selected the elements for the dice and added a 'hidden' class style to make them invisible when game starts

--- Pig Dice Game/script.js
+++ Pig Dice Game/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Getting the elements of the scores
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const diceEl = document.querySelector('.dice')
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
